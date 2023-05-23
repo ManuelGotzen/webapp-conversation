72a91d9f50ded119495e14ea7e2511e0893c0202
Manuel Gotzen <8329497+ManuelGotzen@users.noreply.github.com>
2023-05-23 14:03:59 +0200

    index.ts
    https://udify.app/chat/KuQTLLhzz7uYWNuM
    https://api.dify.ai/v1

--- config/index.ts
+++ config/index.ts
@@ -1,10 +1,10 @@
 import { AppInfo } from "@/types/app"
 export const APP_ID = '533e131f-6768-427b-bbfa-975b8ec9b725'
 export const API_KEY = '<Web API Key From Dify>'
-export const API_URL = ''
+export const API_URL = 'dify.ai'
 export const APP_INFO: AppInfo = {
   "title": 'Chat APP',
-  "description": '',
+  "description": 'https://udify.app/chat/KuQTLLhzz7uYWNuM',
   "copyright": '',
   "privacy_policy": '',
   "default_language": 'zh-Hans'
